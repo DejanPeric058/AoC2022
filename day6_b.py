d7355a2c8450f84b34b14c23d16f1de3909bbf3d
DejanPeric058 <dejan.peric058@gmail.com>
2023-11-24 11:13:55 +0100

    Zaključil dan 6.

---
+++ day6_b.py
@@ -0,0 +1,11 @@
+with open("day6_input.txt") as f:
+    my_string = f.read()
+
+n = 14
+marker = [x for x in my_string[:n-1]]
+for i, charachter in enumerate(my_string[n-1:]):
+    marker.append(charachter)
+    if len(set(marker)) == n:
+        print(i+n)
+        break
+    marker.pop(0)
